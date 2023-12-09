4ecd92e12cded456f44095e78e1d76e2c4ee7e1a
Natcharin Sukcharoensub <natcharin.sc@gmail.com>
2023-12-09 17:43:39 +0700

    save

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("ekapon@moj.go.th");
 });
 
 const PORT = process.env.PORT || 4200;
